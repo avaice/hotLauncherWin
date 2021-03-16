871c311d83ad06335969869718a23940d3b0a382
avaice <80768507+avaice@users.noreply.github.com>
2021-03-17 03:30:45 +0900

    Primary commit
    とりあえず基礎が完成したのでアップする

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,110 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.newsBrowser = new System.Windows.Forms.WebBrowser();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.r2 = new System.Windows.Forms.RadioButton();
+            this.r1 = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.DtoI_interval = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // newsBrowser
+            // 
+            resources.ApplyResources(this.newsBrowser, "newsBrowser");
+            this.newsBrowser.Name = "newsBrowser";
+            // 
+            // PlayButton
+            // 
+            resources.ApplyResources(this.PlayButton, "PlayButton");
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.r2);
+            this.groupBox1.Controls.Add(this.r1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // r2
+            // 
+            resources.ApplyResources(this.r2, "r2");
+            this.r2.Name = "r2";
+            this.r2.UseVisualStyleBackColor = true;
+            // 
+            // r1
+            // 
+            resources.ApplyResources(this.r1, "r1");
+            this.r1.Checked = true;
+            this.r1.Name = "r1";
+            this.r1.TabStop = true;
+            this.r1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.SizingGrip = false;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            resources.ApplyResources(this.StatusLabel, "StatusLabel");
+            // 
+            // DtoI_interval
+            // 
+            this.DtoI_interval.Interval = 800;
+            this.DtoI_interval.Tick += new System.EventHandler(this.DtoI_interval_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.newsBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser newsBrowser;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton r2;
+        private System.Windows.Forms.RadioButton r1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.Timer DtoI_interval;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 namespace hotLauncherWin
 {
@@ -15,6 +17,345 @@ namespace hotLauncherWin
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //--------------------------------------------------------------------
+        //初期宣言（弄らなくて大丈夫です）
+        //--------------------------------------------------------------------
+        System.Net.WebClient downloadClient = null;
+
+
+        //--------------------------------------------------------------------
+        //ゲーム・ダウンロード情報に関する設定項目です！必ず設定してください！
+        //--------------------------------------------------------------------
+
+        //ゲームの最新バージョンを示したtxtファイルのURL
+        private readonly string verURL = ("");
+        //ゲームのバイナリデータの場所を示したtxtファイルのURL
+        private readonly string resourceURL = ("");
+        //このアプリケーションのタイトル
+        private readonly string launcherName = "MyAwesomeGame Updater";
+        //UnityのProject Settings→Product Name
+        private readonly string productName = "MyAwesomeProductName";
+
+        //ゲームの最新情報を表示するWebBrowserのURL
+        private readonly string newsURL = "";
+
+
+
+        //--------------------------------------------------------------------
+        //起動時に呼び出される関数(Form1_Load)※ここで処理を進めます
+        //--------------------------------------------------------------------
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Main.csで指定したランチャー名に変更する
+            this.Text = launcherName;
+
+            //設定項目に抜けが無いかチェックする
+            string chkSettings = VerifySettings();
+            if (chkSettings == "ok") { }else
+            {
+                newsBrowser.DocumentText = ("<center><h1><b>hotLauncher ランチャー設定エラー！</b></h1><br>" + chkSettings + "<br><br><font color=red>このエラーが解決するまでランチャーは動作しません。</font></center>");
+                return;
+            }
+            
+            //最新情報をブラウザで表示する
+            newsBrowser.Url = new Uri(newsURL);
+
+            //アップデートをチェックする
+            string dlResponce = CheckUpdates();
+            switch (dlResponce)
+            {
+                //最新バージョンの時
+                case "latest":
+                    //処理が全て完了した時の関数へ飛ぶ
+                    AllEnd();
+                    return;
+                //エラー時
+                case "error":
+                    StatusLabel.Text = "サーバーエラー！バージョン確認に失敗しました。";
+                    return;
+                //アップデートがある時
+                default:
+                    //StatusLabel.Text = ("Ver" + dlResponce +" アップデートが見つかりました。");
+                    DoUpdate();
+                    break;
+            }
+        }
+
+
+
+        //--------------------------------------------------------------------
+        //処理を行う関数(Public)
+        //--------------------------------------------------------------------
+        
+        string CheckUpdates()
+        {
+            string currentVer;
+
+            //インストール済みVerを記録しているVER.txtが存在するか確認する。無ければcurrentVerに000を代入する
+            if (File.Exists(@"VER.txt"))
+            {
+                currentVer = ReadByTextFile("VER.txt");
+                File.Delete(@"VER.txt");
+            }
+            else { currentVer = "000"; }
+
+            //verURLのダウンロードをする。このif文は「もしエラーを返さずに完了出来たら」。
+            if (DownloadFromURL(verURL, "VER.txt"))
+            {
+                string latestVer = ReadByTextFile("VER.txt");
+                if (currentVer == latestVer)
+                {return "latest";}
+                else
+                {return latestVer;}
+            }
+            else{
+                //エラー発生時
+                //return "error cannot download from: " + verURL;
+                return "error";
+            }
+
+
+        }
+
+        void DoUpdate()
+        {
+            string resURL = GetFromURL(resourceURL);
+            if(resURL == "error")
+            {
+                StatusLabel.Text = "サーバーエラー！ アップデート フェーズ1で失敗しました。";
+                return;
+            }
+            else
+            {
+                DownloadResources(resURL);
+                return;
+            }
+        }
+
+        void ApplyUpdate()
+        {
+            string filePath = System.IO.Directory.GetCurrentDirectory() + @"\" + productName;
+
+            // 以前のバージョンが存在するかどうかを確認する
+            DirectoryInfo di = new DirectoryInfo(filePath);
+            if (di.Exists)
+            {
+                //存在すれば削除する
+                System.IO.Directory.Delete(filePath, true);
+            }
+
+           
+
+            try
+            {
+                //Zip展開処理
+                System.IO.Compression.ZipFile.ExtractToDirectory(@"Resource.zip", System.IO.Directory.GetCurrentDirectory());
+            }
+            catch
+            {
+                //展開失敗
+                StatusLabel.Text = "アップデート適用中にエラーが発生しました。";
+                return;
+            }
+
+            //終わったら
+            AllEnd();
+            
+
+
+        }
+
+
+        //すべて処理が終わったら呼ばれる
+        void AllEnd()
+        {
+            StatusLabel.Text = ("最新バージョンを実行しています。");
+            progressBar1.Value = 100;
+            PlayButton.Enabled = true;
+        }
+
+        //設定項目に抜けがないかチェックする関数
+        string VerifySettings()
+        {
+            string err = "";
+            if(verURL == "")
+            {
+                err = err + "<br>" +  "verURL(バージョン取得URL)を指定してください！";
+            }
+            if (resourceURL == "")
+            {
+                err = err + "<br>" + "resourceURL(リソース情報取得先URL)を指定してください！";
+            }
+            if (launcherName == "")
+            {
+                err = err + "<br>" + "launcherName(ランチャーのタイトル名)を指定してください！";
+            }
+            if (productName == "")
+            {
+                err = err + "<br>" + "productName(ゲームのProduct Name)を指定してください！";
+            }
+            if (newsURL == "")
+            {
+                err = err + "<br>" + "newsURL(最新情報表示WebBrowserのURL)を指定してください！";
+            }
+            if (err == "")
+            {
+                err =  "ok";
+            }
+            return err;
+        }
+
+
+
+        //--------------------------------------------------------------------
+        //モジュール化された関数
+        //--------------------------------------------------------------------
+
+        //txtファイルを読み込んで返す(1行のファイルを読む前提での設計です)
+        string ReadByTextFile(string _file)
+        {
+            //読み込むテキストファイル
+            string textFile = @_file;
+            //文字コード(UTF-8)
+            System.Text.Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+
+            //テキストファイルの中身をすべて読み込む
+            return System.IO.File.ReadAllText(textFile, enc);
+        }
+
+
+        //ダウンロードが出来ればTrue,　失敗ならFalseを返す。
+        //第1引数はダウンロードURL、第2引数は保存ファイル名
+        //※リソースデータのダウンロードは別関数です
+        bool DownloadFromURL(string _url, string _fName)
+        {
+            //ダウンロードが成功したかをBool型で格納する
+            bool isCompleted = true;
+            try
+            {
+                System.Net.WebClient wc = new System.Net.WebClient();
+                wc.DownloadFile(_url, @_fName);
+                wc.Dispose();
+            }
+            catch
+            {
+                //DL失敗
+                isCompleted = false;
+            }
+
+            return isCompleted;
+        }
+
+        string GetFromURL(string _url)
+        {
+            string returnstr;
+            try
+            {
+                WebRequest webReq = WebRequest.Create(_url);
+                Stream ReqStream = webReq.GetResponse().GetResponseStream();
+                StreamReader objReader = new StreamReader(ReqStream);
+                returnstr = objReader.ReadLine();
+            }
+            catch
+            {
+                //DL失敗
+                returnstr = "error";
+            }
+            
+            return returnstr;
+        }
+
+        void DownloadResources(string _url)
+        {
+            //WebClientの作成
+            if (downloadClient == null)
+            {
+                downloadClient = new System.Net.WebClient();
+                //イベントハンドラの作成
+                downloadClient.DownloadProgressChanged +=
+                    new System.Net.DownloadProgressChangedEventHandler(
+                        resDLProgressChanged);
+                downloadClient.DownloadFileCompleted +=
+                    new System.ComponentModel.AsyncCompletedEventHandler(
+                        resDLCompleted);
+            }
+            //非同期ダウンロードを開始する
+            downloadClient.DownloadFileAsync(new Uri(_url), "Resource.zip");
+        }
+
+        //--------------------------------------------------------------------
+        //イベントハンドラ
+        //--------------------------------------------------------------------
+
+        private void resDLProgressChanged(object sender,
+       System.Net.DownloadProgressChangedEventArgs e)
+        {
+            string a = e.ProgressPercentage.ToString();
+            string b = (e.TotalBytesToReceive / 1024000).ToString();
+            string c = (e.BytesReceived / 1024000).ToString();
+            progressBar1.Value = (int)(e.ProgressPercentage * 0.8f);
+            StatusLabel.Text = (a + "% (" + b + "MB 中 " + c + "MB) ダウンロードが終了しました。");
+
+
+
+
+        }
+
+        private void resDLCompleted(object sender,
+            System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+                StatusLabel.Text = ("キャンセルされました。");
+            else if (e.Error != null)
+                StatusLabel.Text = ("ダウンロードエラー！ アップデート フェーズ2で失敗しました。");
+            else
+                StatusLabel.Text = ("アップデートを適用しています・・・・");
+
+            //使い終わったイベントハンドラを消す
+            downloadClient.DownloadProgressChanged -=
+                new System.Net.DownloadProgressChangedEventHandler(
+                    resDLProgressChanged);
+            downloadClient.DownloadFileCompleted -=
+                new System.ComponentModel.AsyncCompletedEventHandler(
+                    resDLCompleted);
+
+            DtoI_interval.Enabled = true;
+
+
+        }
+
+
+        //--------------------------------------------------------------------
+        //各種Formコントロールのイベント
+        //--------------------------------------------------------------------
+
+        private void DtoI_interval_Tick(object sender, EventArgs e)
+        {
+            ApplyUpdate();
+            DtoI_interval.Enabled = false;
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            var screen = System.Windows.Forms.Screen.PrimaryScreen;
+            int width = screen.Bounds.Width;
+            int height = screen.Bounds.Height;
+
+            //ウィンドウ/フルスクリーン　のチェック状態で起動時の引数を変更する
+            if (r1.Checked)
+            {
+                System.Diagnostics.Process.Start(System.IO.Directory.GetCurrentDirectory() + @"\" + productName + @"\" + productName + ".exe", "-screen-fullscreen 0");
+            }
+            if (r2.Checked)
+            {
+                System.Diagnostics.Process.Start(System.IO.Directory.GetCurrentDirectory() + @"\" + productName + @"\" + productName + ".exe", "-screen-fullscreen 1 -screen-width " + width.ToString() + " -screen-height " + height.ToString());
+            }
+
+            //役目を終えたランチャーはこれにて終了
+            this.Close();
         }
     }
 }
